c1154d6a3de888f4bfd17877012c9c677f5b428f
thinkpad <bibinlopez36@gmail.com>
2025-11-11 16:24:05 +0530

    getting the search value

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("wikipedia")
+const formDOM = document.querySelector(".form")
+const inputDOM = document.querySelector(".form-input")
+const resultDOM = document.querySelector(".results")
+
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const value = inputDOM.value
+
+  console.log(value)
+})
