b80c84d8b89be3a8ddcf38c2477e94b5d732dc96
thinkpad <bibinlopez36@gmail.com>
2025-11-11 16:32:41 +0530

    fetch data from the api

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch="
+
 const formDOM = document.querySelector(".form")
 const inputDOM = document.querySelector(".form-input")
 const resultDOM = document.querySelector(".results")
@@ -16,5 +19,14 @@ formDOM.addEventListener("submit", (e) => {
 })
 
 const fetchPages = async (searchValue) => {
-  console.log(searchValue)
+  resultDOM.innerHTML = '<div class= "loading"></div>'
+  try {
+    const response = await fetch(`${url}${searchValue}`)
+    const data = await response.json()
+    const results = data.query.search
+    // renderResults(results)
+    console.log(results)
+  } catch (error) {
+    resultDOM.innerHTML = '<div class="error">there was an error...</div>'
+  }
 }
